0eaf8fe005f87333005d109bec2de99419fbfe0d
BuyKart <buykarthelp@gmail.com>
2025-03-17 12:17:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   authDomain: "tic-toc-8bd3c.firebaseapp.com",
   databaseURL: "https://tic-toc-8bd3c-default-rtdb.firebaseio.com",
   projectId: "tic-toc-8bd3c",
-  storageBucket: "tic-toc-8bd3c.firebasestorage.app",
+  storageBucket: "tic-toc-8bd3c.appspot.com",
   messagingSenderId: "143591558845",
   appId: "1:143591558845:web:976aa26fd5027f0df7f1d4"
 };
